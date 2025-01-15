16db7f0335a4ca778ff09d4f1ff03d541463dcb9
noah <noah@spaceling.sh>
2025-01-15 13:26:31 -0500

    more

--- private_dot_config/private_fish/functions/bup.fish
+++ private_dot_config/private_fish/functions/bup.fish
@@ -1,3 +1,7 @@
 function bup
-    sudo dnf update -y && fisher update && pipx upgrade-all && cargo install trippy
+    sudo dnf update -y \
+        && fisher update \
+        && pipx upgrade-all \
+        && cargo install trippy --locked \
+        && cargo install rmpc --locked
 end
